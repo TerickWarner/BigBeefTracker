7ecbccb55adb8574dc5dee3883cf67714c5f1ce0
TerickWarner <69830220+TerickWarner@users.noreply.github.com>
2021-04-26 19:05:20 -0400

    Add files via upload
    Changed program

--- main.py
+++ main.py
@@ -1,89 +1,66 @@
 from nba_api.stats.static import players
 from nba_api.stats.static import teams
-from nba_api.stats.endpoints import leaguegamefinder, boxscoretraditionalv2
+from nba_api.stats.endpoints import leaguegamefinder, boxscoretraditionalv2, teamgamelog, boxscoresummaryv2
 import pandas
 import csv
+import time
+from datetime import datetime
 
 
+LAST_GAME_IDS = []
+print(LAST_GAME_IDS)
+# STEP 1: CREATE A CSV FILE WITH EACH TEAMS ID NUMBER
+# nba_teams = teams.get_teams()
+# nba_teams_ids = pandas.DataFrame(nba_teams, columns=["id"])
+# nba_teams_ids.to_csv('nba_teams_ids.csv', index=False)
+# time.sleep(10)
 
-#This gets all the teams put in a dictionary
-nba_teams = teams.get_teams()
-#need to get team ids to a csv
-nba_teams_ids = pandas.DataFrame(nba_teams, columns=["id"])
-nba_teams_ids.to_csv('nba_teams_ids.csv', index=False)
+#STEP 2: FIND THE LAST GAME PLAYED BY EACH TEAM
+# time.sleep(2)
+# gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=1610612743)
+# time.sleep(2)
+# game = gamefinder.get_data_frames()[0]
+# last_game = game.head(1)
 
-#now we need to loop through csv to get last game played
-#NOW NEED TO FIGURE OUT HOW TO GET THE LAST GAME PLAYED
-for value in nba_teams_ids['id']:
-    gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=value)
-    games = gamefinder.get_data_frames()[-1]
-    games.head()
+#STEP 3: USE GAME_ID TO FIND DATE
+# time.sleep(2)
+# box_score = boxscoresummaryv2.BoxScoreSummaryV2(game_id=last_game.GAME_ID)
+# game_date = str(box_score.game_info.get_data_frame()['GAME_DATE'][0])
+
+#STEP 4: GET THE CURRENT DAY IN WEEKDAY, MONTH DAY, YEAR FORMAT
+now = datetime.now()
+d2 = now.strftime("%A, %B %d, %Y")
+print(d2.upper())
+
+#STEP 5: LOOP THROUGH THE CSV FILE TO GET TEAM IDS
+with open('nba_teams_ids.csv', newline='') as ids:
+    reader = csv.reader(ids)
+    for row in reader:
+        if row == ['id']:
+            pass
+        else:
+            gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=row)
+            game = gamefinder.get_data_frames()[0]
+            last_game = game.head(1)
+
+            time.sleep(1)
+            box_score_summary = boxscoresummaryv2.BoxScoreSummaryV2(game_id=last_game.GAME_ID)
+            game_date = str(box_score_summary.game_info.get_data_frame()['GAME_DATE'][0])
+            box_score = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=last_game.GAME_ID)
+            player_stats = box_score.player_stats.get_data_frame()
+            if (int(last_game.GAME_ID) not in LAST_GAME_IDS):
+                LAST_GAME_IDS.append(int(last_game.GAME_ID))
+                for (key, value) in player_stats["REB"].items():
+                    if value > 10:
+                        print(f"{player_stats['PLAYER_NAME'][key]} had {int(value)} rebounds ")
+                print(LAST_GAME_IDS)
+            else:
+                print("Game already used")
+            time.sleep(1)
+
+#STEP 6: NOW IF A GAME HAS BEEN USED DON'T USE THAT MATCH UP AGAIN
+#Could we add each game id to a csv that changes and use a simple if x in game_id csv pass
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # for one team we are able to get their ID in order to find their game logs
-# pacers = [team for team in nba_teams if team['abbreviation'] == 'IND'][0]
-# pacers_id = pacers['id']
-#
-#
-# #once we have the idea we use endpoint leaguegamefinder to get dataframe of entire history of games
-# gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=pacers_id)
-# games = gamefinder.get_data_frames()[0]
-# games.head()
-#
-# #we need to eventually get the last game of the team played so we can track if there are any big beefs
-# #We use the sort_values method and get the ending with iloc. FIGURE OUT HOW AND WHY THIS WORKS
-# last_cers_game = games.sort_values('GAME_DATE').iloc[-1]
-#
-#
-# #We get the game ID for the last game the Pacers played. Being able to get the game id from the last game played will
-# #come in handy when we want to get the game id of all games played that night. The .GAME_ID method will return any
-# #game id for a game, just have to make sure it is on the right day.
-# print(last_cers_game.GAME_ID)
-#
-# #we now have a boxscore for the last game the pacers played
-# box_score = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id='0022000882')
 # player_stats = box_score.player_stats.get_data_frame()
-#
-# #prints out the number of rebounds player at index 1 had, need to iterate through
-# # print(int(player_stats["REB"][1]))
-#
-#
-# for (key, value) in player_stats["REB"].items():
-#     if value > 10:
-#         print(f"{player_stats['PLAYER_NAME'][key]} had {int(value)} rebounds ")
-#
-#
-# # for key in player_stats["REB"]:
-# #     if key > 10:
-# #         print(key)
-#
-#
-#
-#
-#
-#
-# # player_dict = players.get_players()
-# #
-# # active_dict = players.get_active_players()
-# # print(active_dict)
-# # #
-# # for key in active_dict:
-# #     print(key['full_name'])
-
-
+# print(player_stats)
